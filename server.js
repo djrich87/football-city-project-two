3408aff963883a01d03dc76d6c793b4038490ce3
djrich87 <djrich87@gmail.com>
2022-03-07 16:56:09 -0700

    commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import logger from 'morgan'
 import methodOverride from 'method-override'
 import passport from 'passport'
 import { passUserToView } from './middleware/middleware.js'
+import { router as profilesRouter } from './routes/profiles.js'
 
 // connect to MongoDB with mongoose
 import('./config/database.js')
@@ -19,6 +20,7 @@ import('./config/passport.js')
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as gamesRouter } from './routes/games.js'
+import { router as profilesRouter } from './routes/profiles.js'
 
 // create the express app
 const app = express()
@@ -64,6 +66,7 @@ app.use(passUserToView)
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
 app.use('/games', gamesRouter)
+app.use('/profiles', profilesRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
