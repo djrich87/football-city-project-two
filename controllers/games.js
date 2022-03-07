f6bfffac8025da87b0051c743966c9f52f8ec805
djrich87 <djrich87@gmail.com>
2022-03-07 13:48:58 -0700

    commit

--- controllers/games.js
+++ controllers/games.js
@@ -42,8 +42,24 @@ function show(req, res) {
   })
 }
 
+function flipAttended(req, res) {
+  Game.findById(req.params.id)
+  .then(game => {
+    game.attended = !game.attended
+    game.save()
+    .then(() => {
+      res.redirect(`/games/${game._id}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/games')
+  })
+}
+
 export {
   index,
   create,
   show,
+  flipAttended,
 }
