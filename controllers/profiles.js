273c4bcb1251f7f4571c60345e01ee9b5c38ba98
djrich87 <djrich87@gmail.com>
2022-03-07 18:39:23 -0700

    add show function to profile controller

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -14,6 +14,27 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .then((profile) => {
+    Profile.findById(req.user.profile._id)
+    .then(self => {
+      const isSelf = self._id.equals(profile._id)
+      res.render('profile/show', {
+        title: `NFL${profile.name}'s profile`,
+        profile,
+        isSelf,
+      })
+    })
+  })
+  .catch((err => {
+    console.log(err)
+    res.redirect("/")
+  }))
+}
+
+
 export {
-  index
+  index,
+  show,
 }
