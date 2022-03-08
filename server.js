22696a9dcf681538c3a13bc877ed89c26c6447c5
djrich87 <djrich87@gmail.com>
2022-03-07 18:12:50 -0700

    mounted './routes/profiles.js'

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ import logger from 'morgan'
 import methodOverride from 'method-override'
 import passport from 'passport'
 import { passUserToView } from './middleware/middleware.js'
-import { router as profilesRouter } from './routes/profiles.js'
 
 // connect to MongoDB with mongoose
 import('./config/database.js')
