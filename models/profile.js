235be8d9b44f236bd50038c033d3d898880cad39
djrich87 <djrich87@gmail.com>
2022-03-07 22:55:58 -0700

    add nflTeams Schema

--- models/profile.js
+++ models/profile.js
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose'
 
+const nflTeamsSchema = new mongoose.Schema({
+  name: String,
+  content: String,
+  city: String,
+}, {
+  timestamps: true
+})
+
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
